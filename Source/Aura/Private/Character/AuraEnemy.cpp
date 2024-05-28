ea5e21434a4630525aa9f439fc661cff3597c5ca
Umut Ozkan <umut.o3n@gmail.com>
2024-05-29 00:29:52 +0300

    Post Process Highlight

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -3,12 +3,38 @@
 
 #include "Character/AuraEnemy.h"
 
+#include "Aura/Aura.h"
+
+AAuraEnemy::AAuraEnemy()
+{
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+	GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	if (Weapon)
+	{
+		Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	}
+}
+
 void AAuraEnemy::HighlightActor()
 {
-	bHighlighted = true;
+	EnableMeshHighlight(GetMesh());
+	EnableMeshHighlight(Weapon);
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
-	bHighlighted = false;
+	DisableMeshHighlight(GetMesh());
+	DisableMeshHighlight(Weapon);
+}
+
+void AAuraEnemy::EnableMeshHighlight(USkeletalMeshComponent* Mesh)
+{
+	if (!Mesh) return;
+	Mesh->SetRenderCustomDepth(true);
+}
+
+void AAuraEnemy::DisableMeshHighlight(USkeletalMeshComponent* Mesh)
+{
+	if (!Mesh) return;
+	Mesh->SetRenderCustomDepth(false);
 }
--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -16,9 +16,11 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 	GENERATED_BODY()
 
 public:
+	AAuraEnemy();
+
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
 
-	UPROPERTY(BlueprintReadOnly)
-	bool bHighlighted = false;
+	static void EnableMeshHighlight(USkeletalMeshComponent* Mesh);
+	static void DisableMeshHighlight(USkeletalMeshComponent* Mesh);
 };
