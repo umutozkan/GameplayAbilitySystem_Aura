3e0b1508bf5bede51d22e0d7da3249917f24b573
Umut Ozkan <umut.o3n@gmail.com>
2024-06-08 02:09:41 +0300

    Initializing Vital Attributes

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -42,6 +42,9 @@ protected:
 	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category="Aura|Attributes")
 	TSubclassOf<UGameplayEffect> DefaultSecondaryAttributes;
 
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category="Aura|Attributes")
+	TSubclassOf<UGameplayEffect> DefaultVitalAttributes;
+
 	void InitializeDefaultAttributes() const;
 	void ApplyEffectToSelf(TSubclassOf<UGameplayEffect> GameplayEffectClass, float Level) const;
 };
