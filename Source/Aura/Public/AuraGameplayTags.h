dcb97ba63d61fbee1cf7f07631768a5fc7b20a88
Umut Ozkan <umut.o3n@gmail.com>
2024-06-09 00:00:33 +0300

    GameplayTags Singleton

---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,23 @@
+// Copyright o3n Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+/**
+ * AuraGameplayTags
+ *
+ * Singleton containing native gameplay tags
+ */
+
+struct FAuraGameplayTags
+{
+public:
+	static const FAuraGameplayTags& Get() { return GameplayTags; }
+	static void InitializeNativeGameplayTags();
+
+protected:
+private:
+	static FAuraGameplayTags GameplayTags;
+};
