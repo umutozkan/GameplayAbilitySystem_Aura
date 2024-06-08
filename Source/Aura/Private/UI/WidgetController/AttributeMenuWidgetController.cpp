89c209ae5d1cc6b90cab72248339b1576bf09a5c
Umut Ozkan <umut.o3n@gmail.com>
2024-06-09 01:24:43 +0300

    Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,16 @@
+// Copyright o3n Games
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	Super::BindCallbacksToDependencies();
+	
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+	Super::BroadcastInitialValues();
+	
+}
