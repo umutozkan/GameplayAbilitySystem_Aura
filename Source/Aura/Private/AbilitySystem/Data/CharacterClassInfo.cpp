7b827024d3a48088af71748fcc2ab7d4f4693cef
Umut Ozkan <umut.o3n@gmail.com>
2024-06-20 00:38:11 +0300

    Character Class Info

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright o3n Games
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
