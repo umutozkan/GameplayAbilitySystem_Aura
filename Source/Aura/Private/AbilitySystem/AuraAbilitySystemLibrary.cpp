1d98fea268e2e9b44d16e45bc6d9967a2b4b9194
Umut Ozkan <umut.o3n@gmail.com>
2024-10-28 00:26:35 +0300

    Melee Polish

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemLibrary.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemLibrary.cpp
@@ -165,3 +165,13 @@ void UAuraAbilitySystemLibrary::GetLivePlayersWithinRadius(const UObject* WorldC
 		}
 	}
 }
+
+bool UAuraAbilitySystemLibrary::IsNotFriend(AActor* FirstActor, AActor* SecondActor)
+{
+	const bool bBothArePlayers = FirstActor->ActorHasTag(FName("Player")) &&
+		SecondActor->ActorHasTag(FName("Player"));
+	const bool bBothAreEnemies = FirstActor->ActorHasTag(FName("Enemy")) &&
+		SecondActor->ActorHasTag(FName("Enemy"));
+	const bool bFriends = bBothAreEnemies || bBothArePlayers;
+	return !bFriends;
+}
