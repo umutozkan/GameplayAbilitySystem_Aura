c1ff5ae5a7d13abaa0984a4cd0b1ecfd10794932
Umut Ozkan <umut.o3n@gmail.com>
2024-11-04 00:40:38 +0300

    Dead Blackboard Key

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -73,6 +73,10 @@ int32 AAuraEnemy::GetPlayerLevel()
 void AAuraEnemy::Die()
 {
 	SetLifeSpan(LifeSpan);
+	if (AuraAIController)
+	{
+		AuraAIController->GetBlackboardComponent()->SetValueAsBool(FName("Dead"), true);
+	}
 	Super::Die();
 }
 
